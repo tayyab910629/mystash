b963ed915ca037dacc9fdfda00b66c2f0ac76046
Tayyab <tayyab910629@gmail.com>
2023-07-17 12:59:55 +0100

    cc

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,3 +15,5 @@ Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
 
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+Rails.application.config.assets.precompile += %w( custom.css )
+
